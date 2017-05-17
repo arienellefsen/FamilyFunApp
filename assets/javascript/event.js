f6dd8af546f5d46e0208b670205d429092f56642
Ariene Ellefsen <ariene.ellefsen@gmail.com>
2017-05-16 20:35:46 -0400

    added markers to map

--- assets/javascript/event.js
+++ assets/javascript/event.js
@@ -1,17 +1,20 @@
+//Create variables for map
 var map;
+var markers = [];
 
+//initialize map
 function initMap() {
     var populateMap = {
         lat: 42.877742,
         lng: -97.380979
     };
+    //Center map in the using USA lat and long coordinates
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: populateMap,
-
     });
 
-
+    //Create a object called family with proprties to be used to search Api eventful 
     var familyFun = {
         keyApi: 'qCDfKZL82TkzJCrr',
         limit: '10',
@@ -19,75 +22,90 @@ function initMap() {
         displayEvent: function(radius, zip, categoryFilter, dateEvent) {
             var urlQuery = 'https://api.eventful.com/json/events/search?location=' + zip + '&within=' + radius +
                 '&q=' + categoryFilter + '&c=' + this.category + '&date=' + dateEvent + '&app_key=' + 'qCDfKZL82TkzJCrr'
-            console.log(urlQuery);
-            console.log("date:" + dateEvent);
             $.ajax({
                 url: urlQuery,
                 method: 'GET',
                 dataType: 'jsonp',
                 success: function(response) {
-                    //Check if we got data from API 
-                    if (response.page_count === '0') {
-                        $('#message-no-result').css('display', 'block');
-                        $('#message-no-result').text('No results found. Please try another zip code');
-                        return;
-                    } else {
-                        $('#message-no-result').css('display', 'none');
-
-                        for (var i = 0; i < 10; i++) {
-
-                            latMap = response.events.event[i].latitude;
-                            longMap = response.events.event[i].longitude;
-
-                            var contentString = '';
-                            contentString += '<div id="contentMap">' + response.events.event[i].title + '</div>';
-                            console.log("lat:" + response.events.event[i].latitude + " long:" + response.events.event[i].longitude);
-                            var builHTML = '';
-                            //console.log("my title: "+response.events.event[i].title);
-                            builHTML += '<div class = "col-md-3 col-sm-4 card">';
-                            //builHTML += '<img src="'+response.events.event[i].image +'" class="img-responsive">';
-                            builHTML += '<b class="title">' + response.events.event[i].title + '</b>';
-                            builHTML += '<p> Event Address:' + response.events.event[i].venue_address + '</p>';
-                            builHTML += '<p> Start Time:' + response.events.event[i].start_time + '</p>';
-                            builHTML += '<p> Postal code:' + response.events.event[i].postal_code + '</p>';
-                            builHTML += ' </div>';
-                            $('#result').append(builHTML);
-                            $('#contentMap').append(builHTML);
-
-
-                            var infowindow = new google.maps.InfoWindow({
-                                content: contentString
-                            });
-
-                            populateMap = new google.maps.LatLng(Number(latMap), Number(longMap));
-                            marker = new google.maps.Marker({
-                                position: populateMap,
-                                map: map
-                            });
-
-                            marker.addListener('click', function() {
-                                infowindow.open(map, marker);
-                            });
-
-                        }
-                        slideContent();
-                        $("#weather").css('display', 'block');
-                        $("#map").css('visibility', 'visible');
-                    }
+                    handleResponse(response);
                 },
             }) // end ajax
         },
     };
 
+    function handleResponse(response) {
+        //Check if we got data from API 
+        //If there is no reponse, display message
+        if (response.page_count === '0') {
+            $('#message-no-result')
+                .css('display', 'block')
+                .text('No results found. Please try another zip code');
+            return;
+        } else {
+            //If we did get the data call build html to display data 
+            $('#message-no-result').css('display', 'none');
+
+            //clean google markers everytime the map is loaded
+            clearMarkers();
+
+            //display map
+            $("#map").css('visibility', 'visible');
+            
+            //loop through results
+            for (var i = 0; i < 10; i++) {
+                latMap = response.events.event[i].latitude;
+                longMap = response.events.event[i].longitude;
+            //build html     
+                var builHTML = '';
+                builHTML += '<div class = "col-md-3 col-sm-4 card">';
+                builHTML += '<b class="title">' + response.events.event[i].title + '</b>';
+                builHTML += '<p> Event Address:' + response.events.event[i].venue_address + '</p>';
+                builHTML += '<p> Start Time:' + response.events.event[i].start_time + '</p>';
+                builHTML += '<p> Postal code:' + response.events.event[i].postal_code + '</p>';
+                builHTML += ' </div>';
+                $('#result').append(builHTML);
+
+            //Call funtion to set markers on map
+                setMapOnAll(map);
+
+            //Populate map with markers
+                populateMap = new google.maps.LatLng(Number(latMap), Number(longMap));
+                var marker = new google.maps.Marker({
+                    position: populateMap,
+                    map: map,
+                });
+            //Push markes to an array markers      
+                markers.push(marker);
+            }
+            //call slide function to display slider
+            slideContent();
+            //Display weather and map html
+            $("#weather").css('display', 'block');
+            $("#map").css('visibility', 'visible');
+        }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+        markers = [];
+    }
+    // Set markers on map
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+            console.log("markers: ", markers[i]);
+        }
+    }
+
     //Create a click event on search button and call display event function
     $("#btnSearch").click(function() {
-
         $('#result').empty();
         $("#weather").empty();
         $("#weather").css('display', 'none');
         $("#map").css('visibility', 'hidden');
-
-
         $("section.slider").removeClass('slick-initialized slick-slider slick-dotted');
         var radius = $('#radius').val();
         var zip = $('#search').val();
@@ -97,9 +115,7 @@ function initMap() {
         if (zip !== '') {
             familyFun.displayEvent(radius, zip, categoryFilter, dateEvent);
             getWeatherInfoByZipCode(zip);
-            $("#map").css('visibility', 'visible');
             resetSearch();
-
             $('#message').css('display', 'none');
         } else {
             console.log('zip cannot be empty');
@@ -108,25 +124,15 @@ function initMap() {
         }
     });
 
+    //Clean input fields for new search 
     function resetSearch() {
-        //Clean input fields for new search       
-        var radius = $('#radius').val('');
+        var radius = $('#radius').val('20');
         var zip = $('#search').val('');
-        var categoryFilter = $('#activities').val('');
+        var categoryFilter = $('#activities').val('museum');
         var dateEvent = $('#event-date').val('');
-
     }
 
-    function resetMap() {
-        var marker = [];
-        for (var i = 0; i < marker.length; i++) {
-            marker[i].setMap(null);
-            console.log("Marker: " + marker[i]);
-        }
-    }
-
-
-    //Call slide
+    //Call library slide slick to create an infinite carousel
     function slideContent() {
         $(".regular").slick({
             dots: true,
