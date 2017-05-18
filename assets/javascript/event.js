b70005626a9840517b67f80d4e5c90324e69806f
Ariene Ellefsen <ariene.ellefsen@gmail.com>
2017-05-18 00:20:12 -0400

    adding final files for project

--- assets/javascript/event.js
+++ assets/javascript/event.js
@@ -1,43 +1,183 @@
-var familyFun = {
-    keyApi: 'qCDfKZL82TkzJCrr',
-    limit:'10',
-    category:'family_fun_kid',
-    displayEvent: function(radius,zip, categoryFilter, dateEvent){
-       var urlQuery= 'https://api.eventful.com/json/events/search?location='+zip+'&within='+radius+
-        '&q='+categoryFilter+'&c='+this.category+'&date='+dateEvent+'&app_key='+'qCDfKZL82TkzJCrr'
-        console.log(urlQuery);
-        console.log("date:" + dateEvent);
-        $.ajax({
-        url: urlQuery,
-         method: 'GET',
-         dataType: 'jsonp',
-         success: function(response){
+//Create variables for map
+var map;
+var markers = [];
 
-             //create html
-             for(var i=0; i < 10; i++){
-                console.log("my title: "+response.events.event[i].title);
-                  $('#result').append("<div class='col-md-4'>");
-                  $('#result').append("<b> Title Event: "+response.events.event[i].title+"</b></div>");
-                  $('#result').append("<p> Event Address: "+response.events.event[i].venue_address+"</p>");
-                  $('#result').append("<p> Start Time: "+response.events.event[i].start_time+"</p>");
-                  $('#result').append("<p> Postal code: "+response.events.event[i].postal_code+"</p>");
-                  $('#result').append("<img src='"+response.events.event[i].img+"'");
-                  $('#result').append("</div>");
-                }
-             }
-        })// end ajax
+//initialize map
+function initMap() {
+    var populateMap = {
+        lat: 42.877742,
+        lng: -97.380979
+    };
+    //Center map in the using USA lat and long coordinates
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: populateMap,
+    });
+
+
+    //Create a object called family with proprties to be used to search Api eventful 
+    var familyFun = {
+        keyApi: 'qCDfKZL82TkzJCrr',
+        limit: '10',
+        category: 'family_fun_kid',
+        displayEvent: function(radius, zip, categoryFilter, dateEvent) {
+            var urlQuery = 'https://api.eventful.com/json/events/search?location=' + zip + '&within=' + radius +
+                '&q=' + categoryFilter + '&c=' + this.category + '&date=' + dateEvent + '&app_key=' + 'qCDfKZL82TkzJCrr'
+            $.ajax({
+                url: urlQuery,
+                method: 'GET',
+                dataType: 'jsonp',
+                success: function(response) {
+                    handleResponse(response);
+                },
+            }) // end ajax
+        },
+    };
+
+    function handleResponse(response) {
+        console.log(response);
+        //Check if we got data from API 
+        //If there is no reponse, display message
+        if (response.page_count === '0') {
+            $('#message-no-result')
+                .css('display', 'block')
+                .text('No results found. Please try another zip code');
+            return;
+        } else {
+            //If we did get the data call build html to display data 
+            $('#message-no-result').css('display', 'none');
+
+            //clean google markers everytime the map is loaded
+            clearMarkers();
+
+            //display map
+            $("#map").css('visibility', 'visible');
+            
+
+
+            //loop through results
+            for (var i = 0; i < 10; i++) {
+               
+
+                latMap = response.events.event[i].latitude;
+                longMap = response.events.event[i].longitude;
+            //build html     
+                var builHTML = '';
+                builHTML += '<div class = "col-md-4">';
+                builHTML += '<div class="card-content">';
+                builHTML += '<h2 class="title">' + response.events.event[i].title + '</h2>';
+                builHTML += '<h1 class="card-date">'+ response.events.event[i].start_time +'</h1>';
+                builHTML += '<p class="card-address"><b>Venue Name</b>: ' + response.events.event[i].venue_name + '</p>';
+                builHTML += '<p class="card-address">' + response.events.event[i].venue_address +' - '+ response.events.event[i].region_abbr +' - ' +response.events.event[i].postal_code +'</p>';
+                builHTML += ' <div class="card-action"><a href="'+response.events.event[i].url +'">Learn More</a>';
+                builHTML += ' </div></div></div>';
+
+
+                $('#result').append(builHTML);
+
+            //Call funtion to set markers on map
+                setMapOnAll(map);
+
+            //Populate map with markers
+                populateMap = new google.maps.LatLng(Number(latMap), Number(longMap));
+                var marker = new google.maps.Marker({
+                    position: populateMap,
+                    map: map,
+                });
+            //Push markes to an array markers      
+                markers.push(marker);
+            }
+            //call slide function to display slider
+            slideContent();
+            //Display weather and map html
+            $("#weather").css('display', 'block');
+            $("#map").css('visibility', 'visible');
+        }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+        markers = [];
+    }
+    // Set markers on map
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+            console.log("markers: ", markers[i]);
+        }
+    }
+
+    //Create a click event on search button and call display event function
+    $("#btnSearch").click(function() {
+        $('#result').empty();
+        $("#weather").empty();
+        $("#weather").css('display', 'none');
+        $("#map").css('visibility', 'hidden');
+        $("section.slider").removeClass('slick-initialized slick-slider slick-dotted');
+        var radius = $('#radius').val();
+        var zip = $('#search').val();
+        var categoryFilter = $('#activities').val();
+        var dateEvent = $('#event-date').val();
+        // Need to validate if fields are not empty
+        if (zip !== '') {
+            familyFun.displayEvent(radius, zip, categoryFilter, dateEvent);
+            getWeatherInfoByZipCode(zip);
+            resetSearch();
+            $('#message').css('display', 'none');
+        } else {
+            console.log('zip cannot be empty');
+            $('#message').css('display', 'block');
+            $('#message').text('Search cannot be empty');
+        }
+    });
+
+    //Clean input fields for new search 
+    function resetSearch() {
+        var radius = $('#radius').val('20');
+        var zip = $('#search').val('');
+        var categoryFilter = $('#activities').val('museum');
+        var dateEvent = $('#event-date').val('');
+    }
+
+    //Call library slide slick to create an infinite carousel
+    function slideContent() {
+        $(".regular").slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 3,
+          
+            responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
     },
-};
-
-//Create a click event on search button and call display event function
-$("#btnSearch").click(function(){
-    var radius = $('#radius').val();
-    var zip = $('#search').val();
-    var  categoryFilter = $('#activities').val();
-    var dateEvent = $('#event-date').val();
-        familyFun.displayEvent(radius,zip, categoryFilter, dateEvent);
- });         
-
-
-
-
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+        });
+    }
+}
