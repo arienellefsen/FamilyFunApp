136a640ced0d4f2a9f2f04fce703f2edb0c3415e
xiaoying wen <xiaoying wen>
2017-05-20 06:46:16 -0400

    updated to clean up

--- assets/javascript/subscribe.js
+++ assets/javascript/subscribe.js
@@ -1,12 +1,6 @@
-/* global firebase moment */
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new train schedules - then update the html + update the database
-// 3. Create a way to retrieve train schedules from the train database upon record been added or deleted from DB
-// 4. adding remove buttons for each train. Let the user delete the train
-// 5. Create a way to calculate the next arrival time and minutes to arrival for the trains every minute. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in current time and the scheduled arrival time.
+//Store subscriber information in FireBase
+//including the subscriber's name,
+//the email address and the zip codes for the interested events' locations
 
 var database = null;
 
@@ -25,7 +19,10 @@ function initUserDB(){
 }
 
 
-//add handler for the Add button been clicked to add the train into DB and the schedule dashboard
+//add handler for the subscribe button that upon click
+//check if the subscriber already exists in the DB by querying with the entered email address
+//For the case of existing subscriber, update the subscriber's zipcode to include the entered zipcode if the zipcode new
+//For the case of new subscriber, create the record with the entered zipcode, name and email address
 function addSubBtnClickListener(){
   $("#subscribe").on("click", function(event) {
     event.preventDefault();
@@ -40,27 +37,24 @@ function addSubBtnClickListener(){
       return;
     }
     console.log('subscribe with' + email + ' for zipcode ' + zipcode);
- $('.modal-title').text('Subscriber successfully added');
+
     var queryWithEmail = database.ref().orderByChild('email').equalTo(email);
     queryWithEmail.once('child_added', function(snapshot){
       //this is an existing user
-      console.log("find key: " + snapshot.key);
+      //console.log("find key: " + snapshot.key);
       var subscribedZipcodes = snapshot.val().zipcode;
-      if(subscribedZipcodes.indexOf(zipcode) != -1){
-        console.log("found zipcode " + zipcode + " in " + subscribedZipcodes);
-      } else {
+      if(subscribedZipcodes.indexOf(zipcode) == -1){
+        //console.log("not found zipcode " + zipcode + " in " + subscribedZipcodes);
         subscribedZipcodes += ',' + zipcode;
-        console.log("update " + snapshot.key + ' with zipcode ' + subscribedZipcodes);
+        //console.log("update " + snapshot.key + ' with zipcode ' + subscribedZipcodes);
         database.ref().child(snapshot.key).update({zipcode: subscribedZipcodes});
+        $('.modal-title').text('Subscribed information successfully updated');
       }
     }); //handle add the data into DB
 
     queryWithEmail.once("value", function(snapshot){
-      if (snapshot.exists()){
-        /*snapshot.forEach(function(child) {
-          ..
-        });*/
-      } else {
+      if (!snapshot.exists()){
+          //this is an new user
           console.log('No child exists');
           var newSubscriber = {
             zipcode: zipcode,
@@ -70,11 +64,9 @@ function addSubBtnClickListener(){
 
           // Uploads employee data to the database
           database.ref().push(newSubscriber);
-            // Alert
-          alert("Subscriber successfully added");
-         
-      } 
-    }); //handle add the data into DB  
+          $('.modal-title').text('Subscriber successfully added');
+      }
+    });
 
     // Clears all of the text-boxes
     $("#zipcode").val("");
@@ -91,5 +83,6 @@ $(document).ready(function(){
   var currentTime = moment([]);
   $('#current-time').text(currentTime.format());
 
+  //add handler to add or update subscriber information
   addSubBtnClickListener();
 });
--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,8 +1,8 @@
 // This is our api.openweathermap.org API key
-    var APIKey = "9653db8b375a737b2906b1e0b371726c";
+var APIKey = "9653db8b375a737b2906b1e0b371726c";
 
-    // Here we are building the URL we need to query the database
-    /*var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+// Here we are building the URL we need to query the database
+/*var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
       "q=Princeton,US&appid=" + APIKey;*/
 
 function getWeatherInfoByZipCode(zipcode){
@@ -16,19 +16,13 @@ function getWeatherInfoByZipCode(zipcode){
       })
       // We store all of the retrieved data inside of an object called "response"
       .done(function(response) {
-
-
-        // Log the resulting object
         $("#zipcode").html("Weather Forecase for " + zipcode + " <small>based on info from station at "  + response.city.name + "</small>");
 
         $.each(response.list, function(index, val){
-            //var checkDate = new Date(this.dt*1000);
-            //var todayFirstTrainTime = moment(todayFirstTrainTimeStr, "YYYY-MM-DDTHH:mm:ss Z");
             var checkDate = moment.unix(this.dt).format("ddd, MM/DD/YY");
             var maxfdegree = Math.round(this.temp.max * 9 / 5 + 32);
             var minfdegree = Math.round(this.temp.min * 9 / 5 + 32);
             var iconid = this.weather[0].icon;
-
 
             $("#weather").append("<tr><td>" 
                 //+ this.weather[0].main 
@@ -39,6 +33,5 @@ function getWeatherInfoByZipCode(zipcode){
                 + maxfdegree + '&deg;/' + minfdegree + "&deg;</td></tr>"); 
       });
     });
-
   }
 
